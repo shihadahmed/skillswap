f55a672e8ca72a2d32c1c3620e3b470cd0c07430
shihadahmed <shihadbinahmed@gmail.com>
2026-08-30 14:27:49 +0600

    fix: add webpack alias config for @/ paths in production build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': require('path').join(__dirname),
+      }
+    }
+    return config
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
